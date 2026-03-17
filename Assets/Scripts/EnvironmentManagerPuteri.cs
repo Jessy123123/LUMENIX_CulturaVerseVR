eb6860ec4a42b74c364a8ebf2e9c62cd2e8831d4
nadh <meera.dhirah@gmail.com>
2026-03-17 15:55:42 +0800

    pushing environment manager

--- Assets/Scripts/EnvironmentManagerPuteri.cs
+++ Assets/Scripts/EnvironmentManagerPuteri.cs
@@ -9,6 +9,24 @@ public class EnvironmentManager : MonoBehaviour
     public GameObject angryEnvironment;
     public ParticleSystem rainParticles; // Drag your rain here in Inspector
 
+    public Material angrySkybox;
+    public Material normalSkybox;
+
+    public Light lightningLight; // Assign this in the Inspector
+
+    // Call this function when you want a flash
+    public void TriggerThunder()
+    {
+        StartCoroutine(FlashLightning());
+    }
+
+    IEnumerator FlashLightning()
+    {
+        lightningLight.enabled = true;
+        yield return new WaitForSeconds(0.1f); // Quick flicker
+        lightningLight.enabled = false;
+    }
+
     public void UpdateEnvironment(string emotion)
     {
         if (emotion == "Sad")
@@ -24,6 +42,8 @@ public class EnvironmentManager : MonoBehaviour
             gloomyEnvironment.SetActive(false);
             angryEnvironment.SetActive(true);
             if (rainParticles != null) rainParticles.Stop();
+            RenderSettings.skybox = angrySkybox; // Changes the sky color
+            DynamicGI.UpdateEnvironment();       // Refreshes the lighting
         }
         else
         {
